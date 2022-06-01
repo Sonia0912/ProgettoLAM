cfea958a44288b50498884695ca2c6eb34e1b2a1
ericdecanini <eddecanini@gmail.com>
2022-06-01 23:41:10 +0200

    Adds QR scanning

--- app/src/main/java/com/sonianicoletti/progettolam/ui/main/joingame/JoingameFragment.kt
+++ app/src/main/java/com/sonianicoletti/progettolam/ui/main/joingame/JoingameFragment.kt
@@ -1,14 +1,21 @@
 package com.sonianicoletti.progettolam.ui.main.joingame
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.sonianicoletti.progettolam.databinding.FragmentJoingameBinding
+import com.sonianicoletti.zxing.ZxingScannerActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +23,14 @@ class JoingameFragment : Fragment() {
 
     private lateinit var binding: FragmentJoingameBinding
     private val viewModel: JoingameViewModel by viewModels()
+
+    private val barcodeLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val intent = result.data
+            val qrCodeContent = intent?.getStringExtra("qrcontent")
+            Toast.makeText(requireContext(), qrCodeContent, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,7 +55,10 @@ class JoingameFragment : Fragment() {
         }
 
         // Partecipa con il QR code
-        // TODO
+        binding.joinByQrButton.setOnClickListener {
+            val intent = Intent(requireContext(), ZxingScannerActivity::class.java)
+            barcodeLauncher.launch(intent)
+        }
 
         observeViewEvents()
         return binding.root
